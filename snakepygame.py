7709a6761a42e5b0aa51f42c0f8cbb4d203bf070
ytang913 <ytang18@terpmail.umd.edu>
2021-04-14 23:27:35 -0700

    updating

--- snakepygame.py
+++ snakepygame.py
@@ -8,7 +8,7 @@ screen_width = 600
 screen_height = 600
 
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption('Snake')
+pygame.display.set_caption('Snake Pygame')
 
 #defining font
 font = pygame.font.SysFont(None, 40)
